a94acbdc3d4f5d4ef7774bd6210f1481c7b6e3d3
Nontenda <Nontenda@bitbucket.org>
2015-02-18 17:06:39 +0100

    Optimization

--- data/changeByMontgomery.js
+++ data/changeByMontgomery.js
@@ -1,17 +1,23 @@
-jQuery.fn.replaceEachOne = function (objective, reposition) {
-    this.contents().each(function(){
-        if (this.nodeType == Node.ELEMENT_NODE) {
-            $(this).replaceEachOne(objective, reposition);
-        } else if (this.nodeType == Node.TEXT_NODE) {
-            var label = document.createElement("label");
-            label.innerHTML = this.nodeValue.replace(objective, reposition);
-            this.parentNode.insertBefore(label, this);
-            this.parentNode.removeChild(this);
-        }
+jQuery.fn.replaceEachOne = function (obj) {
+    this.contents().each(function () {
+        $.each(obj, function (key, value) {
+            if (this.nodeType == Node.ELEMENT_NODE) {
+                $(this).replaceEachOne(obj);
+            } else if (this.nodeType == Node.TEXT_NODE) {
+                var label = document.createElement("label");
+
+                label.innerHTML = this.nodeValue.replace(key, value);
+                this.parentNode.insertBefore(label, this);
+                this.parentNode.removeChild(this);
+            }
+        });
     });
 }
 
-$('body').replaceEachOne("gouvernement", "manoir");
-$('body').replaceEachOne("Gouvernement", "Manoir");
-$('body').replaceEachOne("Manuel Valls", "Brad Montgomery");
-$('body').replaceEachOne("François Hollande", "Brett Montgomery");
+$('body').replaceEachOne(
+    {
+        "gouvernement": "manoir",
+        "Gouvernement": "Manoir",
+        "Manuel Valls": "Brad Montgomery",
+        "François Hollande": "Brett Montgomery"
+    });
